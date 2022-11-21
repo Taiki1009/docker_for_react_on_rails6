0fc509d278f399192840339ca6a04c6da20e8dad
taiki011609 <s.taiki011609@gmail.com>
2022-11-21 20:12:44 +0900

    fix: cors設定, path修正

---
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      before_action :set_post, only: %i[show destroy update]
+
+      def index
+        posts = Post.all.order(:id)
+        render json: posts, status: :ok
+      end
+
+      def show
+        render json: @post, status: :ok
+      end
+
+      def create
+        post = Post.new(post_params)
+        if post.save
+          render json: post, status: :created
+        else
+          render json: post.errors, status: :internal_server_error
+        end
+      end
+
+      def update
+        if @post.update(post_params)
+          render json: @post, status: :created
+        else
+          render json: @post.errors, status: :internal_server_error
+        end
+      end
+
+      def destroy
+        if @post.destroy
+          render json: @post, status: :ok
+        else
+          render json: @post.errors, status: :internal_server_error
+        end
+      end
+
+      private
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :posts
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
 end
